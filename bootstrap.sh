98eef7e970ba22db728e53689c92bda00193f57e
jwoy <jwoyame@usgeo.com>
2017-09-07 23:35:07 -0400

    setup workspace according to go standards

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,9 @@ tar -xf /tmp/go.tar.gz -C $GOTARGET
 rm /tmp/go.tar.gz
 
 # apply environment configuration to the user's .profile
-printf "\n# golang configuration" >> $PROFILE
-printf "\nexport PATH=\$PATH:$GOTARGET/go/bin" >> $PROFILE
-printf "\nexport GOROOT=$GOTARGET/go" >> $PROFILE
-printf "\nexport GOPATH=/vagrant/go\n" >> $PROFILE
+printf "\n" >> $PROFILE
+printf "# golang configuration\n" >> $PROFILE
+printf "export GOROOT=$GOTARGET/go\n" >> $PROFILE
+printf "export GOPATH=/vagrant/go\n" >> $PROFILE
+printf "export PATH=\$PATH:$GOTARGET/go/bin:\$GOPATH/bin\n" >> $PROFILE
 
